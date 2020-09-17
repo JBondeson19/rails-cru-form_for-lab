665aeb067a9235f86db0875c08e453a7038e8d2a
JBondeson19 <jpbond58@gmail.com>
2020-09-17 14:53:04 -0400

    "more updates (n_n)"

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,28 @@
 class ArtistsController < ApplicationController
+
+    def index
+        @artists = Artist.all
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+    end
+
+    def new
+        @artist = Artist.new
+    end
+    
+    def create
+        artist = Artist.create(name: params[:name],
+                                bio: params[:bio])
+        byebug
+        redirect_to artist_path(artist.id)
+    end
+
+    def edit 
+    end
+
+    def update 
+    end
+
 end
